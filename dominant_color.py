32d6e00e98d798e3cff0cc99b4151885d4f2ba87
ppizarror <pablo@ppizarror.com>
2021-11-02 10:46:56 -0300

    Actualiza color de imágenes

--- dominant_color.py
+++
@@ -1,7 +0,0 @@
-from colorthief import ColorThief
-
-color_thief = ColorThief('img/09305524.jpg')
-
-# get the dominant color
-dominant_color = color_thief.get_color(quality=1)
-print('#%02x%02x%02x' % dominant_color)
